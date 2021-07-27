bfb76e8875e24341785ee1f7337812c7b959b980
Binh Do <binhdq@fe.edu.vn>
2021-07-27 11:28:53 +0700

    ok

---
+++ day 3/FGWUtils.js
@@ -0,0 +1,11 @@
+const getCurrentDate = ()=>{
+    const options = {  year: 'numeric', month: 'numeric', day: 'numeric' };
+    return new Date().toLocaleDateString("vi-VN",options);
+   
+}
+const printLog = (msg)=>{
+    console.log(msg)
+}
+
+exports.getCurrentDate = getCurrentDate
+exports.printLog = printLog
---
+++ day 3/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const {getCurrentDate,printLog} = require('./FGWUtils')
+
+app.use(express.urlencoded({extended:true}))
+
+
+app.post('/survey',(req,res)=>{
+    var nameInput = req.body.txtName;
+    res.setHeader('Content-Type','text/html');
+    //res.end('<h1>Thank you ' + nameInput + '</h1>')
+    res.write(`<h1>Thank you ${nameInput} for participating the survey! <h1>`)
+    var now = getCurrentDate();
+    printLog(now)
+    res.end(`Current date: ${now}`)
+})
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/views/home.html')
+})
+
+app.get('/about',(req,res)=>{
+    res.setHeader('Content-Type','text/html');
+    res.end('<h1 style="color:red">About page</h1>')
+})
+
+const PORT = 5000;
+app.listen(PORT);
+console.log('Server is running on Port: ',PORT)
