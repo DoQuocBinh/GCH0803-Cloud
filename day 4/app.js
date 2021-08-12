c5155b2fe84b94bac180445c316d1ae4ef8b6792
Binh Do <binhdq@fe.edu.vn>
2021-08-12 08:24:13 +0700

    refactoring code

--- day 4/app.js
+++ day 4/app.js
@@ -1,8 +1,9 @@
 const EXPRESS = require('express')
-const {ObjectId,MongoClient, Int32} = require('mongodb')
+const { Int32} = require('mongodb');
+
+const { insertStudent,deleteStudent,searchStudent,getAllStudent} = require('./databaseHandler');
 
 const APP = EXPRESS()
-const url = 'mongodb://localhost:27017';
 
 APP.use(EXPRESS.urlencoded({extended:true}))
 APP.set('view engine','hbs')
@@ -11,32 +12,23 @@ APP.post('/insert',async (req,res)=>{
     const nameInput = req.body.txtName;
     const tuoiInput = req.body.txtTuoi;
     const newStudent = {name:nameInput,tuoi: Int32(tuoiInput)};
-    const client= await MongoClient.connect(url);
-    const dbo = client.db("GCH0803DB");
-    const newS = await dbo.collection("students").insertOne(newStudent);
-    console.log("Gia tri id moi duoc insert la: ",newS.insertedId.toHexString());
+    await insertStudent(newStudent);
     //chuyen huong den file Index
     res.redirect('/');
 })
 APP.get('/delete',async (req,res)=>{
     const idInput = req.query.id;
-    const client= await MongoClient.connect(url);
-    const dbo = client.db("GCH0803DB");
-    await dbo.collection("students").deleteOne({_id:ObjectId(idInput)})
+    await deleteStudent(idInput);
     res.redirect('/');
 })
 APP.post('/search',async (req,res)=>{
     const searchInput = req.body.txtSearch;
-    const client= await MongoClient.connect(url);
-    const dbo = client.db("GCH0803DB");
-    const allStudents = await dbo.collection("students").find({name:searchInput}).toArray();
+    const allStudents = await searchStudent(searchInput);
     res.render('index',{data:allStudents})
 })
 
 APP.get('/',async (req,res)=>{
-    const client= await MongoClient.connect(url);
-    const dbo = client.db("GCH0803DB");
-    const allStudents = await dbo.collection("students").find({}).toArray();
+    const allStudents = await getAllStudent();
     res.render('index',{data:allStudents})
 })
 
---
+++ day 4/databaseHandler.js
@@ -0,0 +1,33 @@
+const {MongoClient,ObjectId} = require('mongodb');
+
+const URL = 'mongodb://localhost:27017';
+const DATABASE_NAME = "GCH0803DB"
+
+async function getDB() {
+    const client = await MongoClient.connect(URL);
+    const dbo = client.db(DATABASE_NAME);
+    return dbo;
+}
+
+async function insertStudent(newStudent) {
+    const dbo = await getDB();
+    const newS = await dbo.collection("students").insertOne(newStudent);
+    console.log("Gia tri id moi duoc insert la: ", newS.insertedId.toHexString());
+}
+
+async function deleteStudent(idInput) {
+    const dbo = await getDB();
+    await dbo.collection("students").deleteOne({ _id: ObjectId(idInput) });
+}
+async function searchStudent(searchInput) {
+    const dbo = await getDB();
+    const allStudents = await dbo.collection("students").find({ name: searchInput }).toArray();
+    return allStudents;
+}
+async function getAllStudent() {
+    const dbo = await getDB();
+    const allStudents = await dbo.collection("students").find({}).toArray();
+    return allStudents;
+}
+
+module.exports = {getDB,insertStudent,deleteStudent,searchStudent,getAllStudent}
