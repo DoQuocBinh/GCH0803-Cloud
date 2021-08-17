ec6445c68bb5994ee09714712db954c18890d97d
Binh Do <binhdq@fe.edu.vn>
2021-08-17 10:05:24 +0700

    middleware

--- day 4/app.js
+++ day 4/app.js
@@ -2,12 +2,12 @@ const EXPRESS = require('express')
 const { Int32} = require('mongodb');
 const session = require('express-session')
 
-const { insertStudent,deleteStudent,searchStudent,getAllStudent,getStudentById,updateStudent} = require('./databaseHandler');
+const {checkUserRole, insertUser,insertStudent,deleteStudent,searchStudent,getAllStudent,getStudentById,updateStudent} = require('./databaseHandler');
 
 const APP = EXPRESS()
 
 // Use the session middleware
-APP.use(session({ secret: '124447yd@@$%%#', cookie: { maxAge: 60000 }}))
+APP.use(session({ secret: '124447yd@@$%%#', cookie: { maxAge: 60000 },saveUninitialized:false,resave:false}))
 
 APP.use(EXPRESS.urlencoded({extended:true}))
 APP.set('view engine','hbs')
@@ -28,13 +28,14 @@ APP.get('/edit',async (req,res)=>{
 APP.get('/login',(req,res)=>{
     res.render('login')
 })
-APP.post('/doLogin',(req,res)=>{
+APP.post('/doLogin',async (req,res)=>{
     const nameInput = req.body.txtName;
     const passInput = req.body.txtPassword;
-    if(nameInput=="tom" && passInput =="123"){
+    const userRole = await checkUserRole(nameInput,passInput);
+    if(userRole!="-1"){
         req.session["User"]={
             name : nameInput,
-            role : 'admin'
+            role : userRole
         }
     }
     res.redirect('/');
@@ -60,12 +61,32 @@ APP.post('/search',async (req,res)=>{
     res.render('index',{data:allStudents})
 })
 
-APP.get('/',async (req,res)=>{
-    if(!req.session["User"])
-        res.redirect('/login')
+APP.get('/' ,requiresLogin, async (req,res)=>{
+    // if(!req.session["User"])
+    //     res.redirect('/login')
     const allStudents = await getAllStudent();
     res.render('index',{data:allStudents,user:req.session["User"]})
 })
+APP.post('/register',async (req,res)=>{
+    const nameInput = req.body.txtName;
+    const passInput = req.body.txtPassword;
+    const roleInput = req.body.role;
+    await insertUser({name:nameInput,pass:passInput,role:roleInput})
+    res.redirect('/login')
+})
+
+APP.get('/nologin',requiresLogin, (req,res)=>{
+    res.render('noLogin');
+})
+
+//custom middleware
+function requiresLogin(req,res,next){
+    if(req.session["User"]){
+        return next()
+    }else{
+        res.redirect('/login')
+    }
+}
 
 const PORT = process.env.PORT || 5000;
 APP.listen(PORT);
--- day 4/databaseHandler.js
+++ day 4/databaseHandler.js
@@ -9,6 +9,12 @@ async function getDB() {
     return dbo;
 }
 
+async function insertUser(user) {
+    const dbo = await getDB();
+    const newS = await dbo.collection("users").insertOne(user);
+    console.log("Gia tri id moi duoc insert la: ", newS.insertedId.toHexString());
+}
+
 async function insertStudent(newStudent) {
     const dbo = await getDB();
     const newS = await dbo.collection("students").insertOne(newStudent);
@@ -40,5 +46,15 @@ async function updateStudent(id,nameInput,tuoiInput){
     dbo.collection("students").updateOne({_id:ObjectId(id)},{$set:{name:nameInput,tuoi:tuoiInput}})
 
 }
+async function checkUserRole(nameI,passI){
+    const dbo = await getDB();
+    const user= await dbo.collection("users").findOne({name:nameI,pass:passI});
+    if (user==null) {
+        return "-1"
+    }else{
+        console.log(user)
+        return user.role;
+    }
+}
 
-module.exports = {getDB,insertStudent,deleteStudent,searchStudent,getAllStudent,getStudentById,updateStudent}
+module.exports = {getDB,insertStudent,deleteStudent,searchStudent,getAllStudent,getStudentById,updateStudent,insertUser,checkUserRole}
